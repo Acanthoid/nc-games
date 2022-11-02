2ec1c4e5e38b73f5a0d7cb4ee81b2739e56f520b
Adam <linfordaj@gmail.com>
2022-11-02 11:49:03 +0000

    Add on picture click to see specific reviews in detail. css incoming

--- src/Api.js
+++ src/Api.js
@@ -7,3 +7,8 @@ export const getCategory =(category_slug) => {
 return fetch(`https://game-reviews-rated.herokuapp.com/api/reviews/?category=${category_slug}`)
 .then((res) => res.json());
 };
+
+export const getReview = (review_id) => {
+return fetch(`https://game-reviews-rated.herokuapp.com/api/reviews/${review_id}`)
+.then((res) => res.json());
+}
