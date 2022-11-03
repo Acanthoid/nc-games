6e5f33818752a5c4b4068f180b279fc295b37279
Adam <linfordaj@gmail.com>
2022-11-03 09:26:17 +0000

    fix the navbar to remove hardcoding

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { getNavs } from "../Api";
 
 
 const NavBar = () => {
-    getNavs().then(({categories}) => {
-        (
-            <nav className="navBar">
-            {categories.map(
-            (
-                category
-            ) => {
-                const category_link = `/categories/${category.slug}`
-                return (
-                    <li key={category.slug}>
-                        <Link to={category_link}>category</Link>
-                    </li>
-                )
-            }
-            )}
-            </nav>
-        )
-    })
-};
+
+    const [categories, setCategories] = useState([])
     
+    getNavs().then(({categories}) => {setCategories(categories)})
+    return(
+    <nav className="navBar"><ul>
+    {categories.map((category) => {
+        return(
+        <li key={category.slug}>
+            <Link to={`/categories/${category.slug}`}>{category.slug}</Link>
+        </li>)
+    })  }          
+    </ul></nav>
+    )
+}
+        
 export default NavBar;
